fa3a31cc1c5b2f61f186313f805fb9d820bc1b62
Jack Pearce <jrgpearce@gmail.com>
2020-07-19 16:19:05 +0100

    removed deprecated api for modmenu

--- src/main/java/red/jackf/whereisit/compat/ModMenuHandler.java
+++ src/main/java/red/jackf/whereisit/compat/ModMenuHandler.java
@@ -11,11 +11,6 @@ import red.jackf.whereisit.WhereIsItConfig;
 @Environment(EnvType.CLIENT)
 public class ModMenuHandler implements ModMenuApi {
     @Override
-    public String getModId() {
-        return WhereIsIt.MODID;
-    }
-
-    @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
         return (parent) -> AutoConfig.getConfigScreen(WhereIsItConfig.class, parent).get();
     }
