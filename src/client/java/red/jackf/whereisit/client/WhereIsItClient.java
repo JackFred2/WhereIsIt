9cacf5d6441a86d1c6b8fec7991fa6bca2accd33
Jack Pearce <jack@jackf.red>
2023-06-11 15:10:59 +0100

    Initial Commit

---
+++ src/client/java/red/jackf/whereisit/client/WhereIsItClient.java
@@ -0,0 +1,10 @@
+package red.jackf.whereisit.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WhereIsItClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
--- src/main/java/red/jackf/whereisit/WhereIsIt.java
+++ src/main/java/red/jackf/whereisit/WhereIsIt.java
@@ -1,97 +1,22 @@
 package red.jackf.whereisit;
 
-import me.shedaniel.autoconfig.AutoConfig;
-import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
-import net.fabricmc.api.EnvType;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
-import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.item.Items;
-import net.minecraft.text.Text;
-import net.minecraft.text.TranslatableTextContent;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Formatting;
-import net.minecraft.util.Identifier;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import red.jackf.whereisit.network.FoundS2C;
-import red.jackf.whereisit.network.SearchC2S;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class WhereIsIt implements ModInitializer {
-    public static final String MODID = "whereisit";
-    private static final Logger LOGGER = LogManager.getLogger();
-    private static final Map<UUID, Long> rateLimitMap = new HashMap<>();
-    public static WhereIsItConfig CONFIG = AutoConfig.register(WhereIsItConfig.class, GsonConfigSerializer::new).getConfig();
-    public static boolean REILoaded = false;
-    public static boolean EMILoaded = false;
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+    public static final Logger LOGGER = LoggerFactory.getLogger("whereisit");
 
-    public static Identifier id(String path) {
-        return new Identifier(MODID, path);
-    }
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
 
-    public static void log(String str) {
-        LOGGER.info(str);
-    }
-
-    public static void error(String str) {
-        LOGGER.error(str);
-    }
-
-    public static void error(Exception ex) {
-        LOGGER.error(ex);
-    }
-
-    @Override
-    public void onInitialize() {
-        AutoConfig.getConfigHolder(WhereIsItConfig.class).registerSaveListener((configHolder, whereIsItConfig) -> {
-            whereIsItConfig.validatePostLoad();
-            return ActionResult.PASS;
-        });
-
-        if (FabricLoader.getInstance().isModLoaded("roughlyenoughitems")) {
-            REILoaded = true;
-            log("REI Found");
-        }
-
-        if (FabricLoader.getInstance().isModLoaded("emi")) {
-            EMILoaded = true;
-            log("EMI Found");
-        }
-
-        ServerPlayNetworking.registerGlobalReceiver(SearchC2S.ID, ((server, player, handler, buf, responseSender) -> {
-            var searchContext = SearchC2S.read(buf);
-            var itemToFind = searchContext.item();
-            if (itemToFind != Items.AIR) {
-                server.execute(() -> {
-
-                    var basePos = player.getBlockPos();
-                    var world = player.getWorld();
-
-                    long beforeTime = System.nanoTime();
-
-                    if (FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT || world.getTime() >= rateLimitMap.getOrDefault(player.getUuid(), 0L) + WhereIsIt.CONFIG.getCooldown()) {
-                        var positions = Searcher.searchWorld(basePos, world, itemToFind, searchContext.tag(), searchContext.maximum());
-                        if (positions.size() > 0) {
-                            var packet = new FoundS2C(positions);
-                            ServerPlayNetworking.send(player, FoundS2C.ID, packet);
-                            player.closeHandledScreen();
-                        }
-                        rateLimitMap.put(player.getUuid(), world.getTime());
-                    } else {
-                        player.sendMessage(Text.translatable("whereisit.slowDown").formatted(Formatting.YELLOW), false);
-                    }
-
-                    if (WhereIsIt.CONFIG.printSearchTime()) {
-                        long time = (System.nanoTime() - beforeTime);
-                        player.sendMessage(Text.literal("Lookup Time: " + time + "ns"), false);
-                        WhereIsIt.LOGGER.info("Lookup Time: " + time + "ns");
-                    }
-                });
-            }
-        }));
-    }
+		LOGGER.info("Hello Fabric world!");
+	}
 }
