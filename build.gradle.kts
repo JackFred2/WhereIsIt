1bd2d92bdcf9fdcbb2d46e946db1e3e17e91b500
Jack Pearce <jack@jackf.red>
2023-08-30 02:46:33 +0100

    [internal] fix run configs

--- build.gradle.kts
+++ build.gradle.kts
@@ -121,10 +121,26 @@ loom {
 		}
 	}
 
-	runConfigs {
-		configureEach {
-			val path = buildscript.sourceFile?.parentFile?.resolve("log4j2.xml")
-			path?.let { property("log4j2.configurationFile", path.path) }
+	log4jConfigs.from(file("log4j2.xml"))
+
+	runs {
+
+		getByName("client") {
+			environment = "client"
+			configName = "Run Client"
+			runDir = "run"
+			source(sourceSets["client"])
+			ideConfigGenerated(true)
+			client()
+		}
+
+		getByName("server") {
+			environment = "server"
+			configName = "Run Server"
+			runDir = "runServer"
+			source(sourceSets["main"])
+			ideConfigGenerated(true)
+			server()
 		}
 	}
 
