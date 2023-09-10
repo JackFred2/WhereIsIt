1072337a3a0f0317284b8aedf50f86c88f81324d
Jack Pearce <jack@jackf.red>
2023-09-10 13:59:53 +0100

    [feat] optimize default searcher by ~2.5x

--- src/main/java/red/jackf/whereisit/defaults/DefaultBlockSearchers.java
+++ src/main/java/red/jackf/whereisit/defaults/DefaultBlockSearchers.java
@@ -1,9 +1,6 @@
 package red.jackf.whereisit.defaults;
 
 import net.fabricmc.fabric.api.transfer.v1.item.ItemStorage;
-import net.fabricmc.fabric.api.transfer.v1.item.ItemVariant;
-import net.fabricmc.fabric.api.transfer.v1.storage.Storage;
-import net.minecraft.core.Direction;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.Nameable;
 import net.minecraft.world.item.ItemStack;
@@ -14,8 +11,6 @@ import red.jackf.whereisit.api.SearchRequest;
 import red.jackf.whereisit.api.SearchResult;
 import red.jackf.whereisit.api.search.BlockSearcher;
 import red.jackf.whereisit.config.WhereIsItConfig;
-
-import java.util.HashSet;
 
 public class DefaultBlockSearchers {
     static void setup() {
@@ -29,22 +24,17 @@ public class DefaultBlockSearchers {
     private static void setupTransferApi() {
         BlockSearcher.EVENT.register(BlockSearcher.FALLBACK, (request, player, level, state, pos) -> {
             if (!WhereIsItConfig.INSTANCE.getConfig().getCommon().enableDefaultSearchers) return ResultHolder.pass();
-            var checked = new HashSet<Storage<ItemVariant>>();
-            for (var direction : Direction.values()) { // each side
-                var storage = ItemStorage.SIDED.find(level, pos, direction);
-                if (storage != null && !checked.contains(storage)) { // side valid and we haven't already tried this inv
-                    checked.add(storage);
-                    for (var view : storage) { // for each view in this side
-                        if (view.isResourceBlank()) continue;
-                        var resource = view.getResource().toStack((int) view.getAmount()); // get stack
+            var storage = ItemStorage.SIDED.find(level, pos, state, null, null);
+            if (storage != null) { // storage exists here
+                for (var view : storage.nonEmptyViews()) { // for each view in this side
+                    var resource = view.getResource().toStack((int) view.getAmount()); // get stack
 
-                        if (SearchRequest.check(resource, request)) { // if stack passes test
-                            var result = SearchResult.builder(pos);
-                            result.item(resource);
-                            if (level.getBlockEntity(pos) instanceof Nameable nameable)
-                                result.name(nameable.getCustomName(), null);
-                            return ResultHolder.value(result.build());
-                        }
+                    if (SearchRequest.check(resource, request)) { // if stack passes test
+                        var result = SearchResult.builder(pos);
+                        result.item(resource);
+                        if (level.getBlockEntity(pos) instanceof Nameable nameable)
+                            result.name(nameable.getCustomName(), null);
+                        return ResultHolder.value(result.build());
                     }
                 }
             }
