28b43809415dd404dd73a34ee44fb2e16e482d92
Jack Pearce <jack@jackf.red>
2023-10-12 18:12:21 +0100

    [fix] update JFLIB dep

--- src/main/java/red/jackf/whereisit/serverside/ServerSideRenderer.java
+++ src/main/java/red/jackf/whereisit/serverside/ServerSideRenderer.java
@@ -1,7 +1,7 @@
 package red.jackf.whereisit.serverside;
 
-import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.Multimap;
+import com.google.common.collect.MultimapBuilder;
 import com.mojang.authlib.GameProfile;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
@@ -12,11 +12,11 @@ import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.phys.Vec3;
 import org.joml.Vector3f;
 import red.jackf.jackfredlib.api.colour.Colour;
-import red.jackf.jackfredlib.api.lying.ActiveLie;
 import red.jackf.jackfredlib.api.lying.Debris;
-import red.jackf.jackfredlib.api.lying.Lies;
+import red.jackf.jackfredlib.api.lying.Lie;
 import red.jackf.jackfredlib.api.lying.entity.EntityLie;
 import red.jackf.jackfredlib.api.lying.entity.builders.EntityBuilders;
+import red.jackf.jackfredlib.api.lying.entity.builders.display.BlockDisplayBuilder;
 import red.jackf.whereisit.WhereIsIt;
 import red.jackf.whereisit.api.SearchResult;
 import red.jackf.whereisit.config.WhereIsItConfig;
@@ -32,47 +32,46 @@ public class ServerSideRenderer {
     private static final Block MAIN_BLOCK = Blocks.STRUCTURE_BLOCK;
     private static final Block ALT_BLOCK = Blocks.JIGSAW;
     private static final float HIGHLIGHT = 0.7f;
-    private static final Multimap<GameProfile, ActiveLie<EntityLie<Display.BlockDisplay>>> playerHighlightLies = ArrayListMultimap.create();
+    private static final Multimap<GameProfile, EntityLie<Display.BlockDisplay>> playerHighlightLies = MultimapBuilder.hashKeys()
+            .arrayListValues().build();
 
     /**
      * Fade all server-side lies for a player
+     *
      * @param player Player to fade lies for
      */
     public static void fadeServerSide(ServerPlayer player) {
-        playerHighlightLies.removeAll(player.getGameProfile()).forEach(ActiveLie::fade);
+        playerHighlightLies.removeAll(player.getGameProfile()).forEach(Lie::fade);
     }
 
     /**
      * Build an entity lie to show the given player, using ALT_BLOCK as the block.
-     * @param level Level to place the lie in
-     * @param pos Position to place the lie at
+     *
+     * @param level  Level to place the lie in
+     * @param pos    Position to place the lie at
      * @param colour Colour of the lie
      * @return Built entity lie at the given position
      */
-    private static EntityLie<Display.BlockDisplay> makeAlternateBlockDisplay(ServerLevel level, BlockPos pos, Colour colour) {
-        return EntityLie.builder(EntityBuilders.blockDisplay(level)
-                        .positionCentered(pos)
-                        .state(ALT_BLOCK.defaultBlockState())
-                        .scaleAndCenter(HIGHLIGHT)
-                        .glowing(true, colour)
-                        .build())
-                .onFade(activeLie -> playerHighlightLies.get(activeLie.player().getGameProfile()).remove(activeLie))
-                .build();
+    private static Display.BlockDisplay makeAlternateBlockDisplay(ServerLevel level, BlockPos pos, Colour colour) {
+        return EntityBuilders.blockDisplay(level).positionCentered(pos).state(ALT_BLOCK.defaultBlockState())
+                .scaleAndCenter(HIGHLIGHT).glowing(true, colour).build();
     }
 
     /**
      * Return a random fade time, centered on the config time with some jitter
+     *
      * @return Randomly generated fade time
      */
     private static int randomFadeTime() {
-        var baseTime = WhereIsItConfig.INSTANCE.instance().getCommon().fadeoutTimeTicks;
+        int baseTime = WhereIsItConfig.INSTANCE.instance().getCommon().fadeoutTimeTicks;
         int random = (int) ((4 * TICKS_PER_SECOND) * Math.random() - (2 * TICKS_PER_SECOND));
         return baseTime + random;
     }
 
     /**
      * Render a set of results to the player, using JackFredLib's lying module
-     * @param player Player to render server-side results for
+     *
+     * @param player  Player to render server-side results for
      * @param results Results to render
      */
     public static void doServersideRendering(ServerPlayer player, Collection<SearchResult> results) {
@@ -80,33 +79,36 @@ public class ServerSideRenderer {
         var level = (ServerLevel) player.level();
 
         for (SearchResult result : results) {
-            var colour = Colour.fromHSV((float) Math.random(), 1, 1);
+            Colour colour = Colour.fromHSV((float) Math.random(), 1, 1);
 
-            var timeout = randomFadeTime();
+            int timeoutTicks = randomFadeTime();
 
-            var mainEntity = EntityBuilders.blockDisplay(level)
-                    .position(Vec3.atBottomCenterOf(result.pos().above())
-                            .add(result.name() != null ? result.nameOffset().subtract(0, 1, 0) : Vec3.ZERO))
-                    .state(MAIN_BLOCK.defaultBlockState())
-                    .scaleAndCenter(HIGHLIGHT)
-                    .addTranslation(new Vector3f(0, -0.5f, 0))
-                    .glowing(true, colour);
+            Vec3 mainPos = Vec3.atBottomCenterOf(result.pos().above());
+            if (result.name() != null) mainPos = mainPos.add(result.nameOffset().subtract(0, 1, 0));
+
+            BlockDisplayBuilder mainEntity = EntityBuilders.blockDisplay(level).position(mainPos)
+                    .state(MAIN_BLOCK.defaultBlockState()).scaleAndCenter(HIGHLIGHT)
+                    .addTranslation(new Vector3f(0, -0.5f, 0)).glowing(true, colour);
 
             if (result.name() != null) {
                 mainEntity.customName(result.name()).alwaysRenderName(true);
                 mainEntity.addTranslation(result.nameOffset().subtract(0, 1, 0).reverse().toVector3f());
             }
 
-            var active = Lies.INSTANCE.addEntity(player, EntityLie.builder(mainEntity.build())
-                    .onFade(activeLie -> playerHighlightLies.get(activeLie.player().getGameProfile()).remove(activeLie))
-                    .build());
-            playerHighlightLies.put(player.getGameProfile(), active);
-            Debris.INSTANCE.schedule(active, timeout);
+            EntityLie<Display.BlockDisplay> main = EntityLie.builder(mainEntity.build())
+                    .onFade((player1, lie) -> playerHighlightLies.remove(player1.getGameProfile(), lie))
+                    .createAndShow(player);
+
+            playerHighlightLies.put(player.getGameProfile(), main);
+
+            Debris.INSTANCE.schedule(main, timeoutTicks);
 
             for (BlockPos otherPos : result.otherPositions()) {
-                var proxy = Lies.INSTANCE.addEntity(player, makeAlternateBlockDisplay(level, otherPos, colour));
+                EntityLie<Display.BlockDisplay> proxy = EntityLie.builder(makeAlternateBlockDisplay(level, otherPos, colour))
+                        .onFade((player1, lie) -> playerHighlightLies.remove(player1.getGameProfile(), lie))
+                        .createAndShow(player);
                 playerHighlightLies.put(player.getGameProfile(), proxy);
-                Debris.INSTANCE.schedule(proxy, timeout);
+                Debris.INSTANCE.schedule(proxy, timeoutTicks);
             }
         }
     }
